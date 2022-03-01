63f8e76adc7769346974e60365d1848509b33cba
Hedgehawk11 <84346970+dbates314@users.noreply.github.com>
2022-03-01 17:01:14 +0000

    ready for "if"

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame as game
+import sys as s
 game.init()
 print('\n' * 1000)
-print('awncer these questions with Y OR N')
+print('awncer these questions with Y OR N\n')
 
 
 rick = input('do you like "never gonna give you up"')
+roll = input('do you like rolling')
+spam = input('do you like spam (email)')
+montyspam = input('do you like monty python\'s skit "spam"')
+games = input('do you like games')
 
